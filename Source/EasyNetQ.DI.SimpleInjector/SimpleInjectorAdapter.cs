4c0003d388613e6be59720e6e854152010330c19
Robert Hager <robert@hagers.ws>
2014-04-30 19:52:25 -0500

    added SimpleInjector support for internal DI

---
+++ Source/EasyNetQ.DI.SimpleInjector/SimpleInjectorAdapter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using SimpleInjector;
+
+namespace EasyNetQ.DI
+{
+    public class SimpleInjectorAdapter : IContainer
+    {
+        private readonly Container _container;
+
+        public SimpleInjectorAdapter(Container container)
+        {
+            _container = container;
+        }
+
+        public TService Resolve<TService>() where TService : class
+        {
+            return _container.GetInstance<TService>();
+        }
+
+        public IServiceRegister Register<TService>(Func<IServiceProvider, TService> serviceCreator) where TService : class
+        {
+            if(!IsRegistered<TService>())
+                _container.RegisterSingle(() => serviceCreator(this));
+            
+            return this;
+        }
+
+        public IServiceRegister Register<TService, TImplementation>() where TService : class where TImplementation : class, TService
+        {
+            if(!IsRegistered<TService>())           
+                _container.RegisterSingle<TService, TImplementation>();
+            
+            return this;
+        }
+
+        private bool IsRegistered<TService>()
+        {
+            return _container.GetCurrentRegistrations().Any(x => x.ServiceType == typeof(TService));
+        }
+    }
+}
---
+++ Source/EasyNetQ.DI.Tests/SimpleInjectorAdapterTests.cs
@@ -0,0 +1,39 @@
+﻿using EasyNetQ.Tests.Mocking;
+using NUnit.Framework;
+using SimpleInjector;
+
+namespace EasyNetQ.DI.Tests
+{
+    [TestFixture]
+    [Explicit("Starts a connection to localhost")]
+    public class SimpleInjectorAdapterTests
+    {
+        private Container container;
+        private IBus bus;
+
+        [SetUp]
+        public void SetUp()
+        {
+            container = new Container();
+            container.RegisterAsEasyNetQContainerFactory();
+
+            bus = new MockBuilder().Bus;
+        }
+
+        [Test]
+        public void Should_create_bus_with_simple_injector_adapter()
+        {
+            Assert.IsNotNull(bus);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (bus != null)
+            {
+                bus.Dispose();
+            }
+            RabbitHutch.SetContainerFactory(() => new DefaultServiceProvider());
+        }
+    }
+}
